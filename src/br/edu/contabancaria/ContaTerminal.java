17b56bcd3950b03e607c4796a3a2787bb7d80a55
AlexsanderGS <alexsanderga@gmail.com>
2023-09-21 19:01:02 -0300

    Finalizando o desafio

--- src/br/edu/contabancaria/ContaTerminal.java
+++ src/br/edu/contabancaria/ContaTerminal.java
@@ -1,9 +1,33 @@
 package br.edu.contabancaria;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Por favor, digite o número da Conta: ");
+		int numero = sc.nextInt();
+
+		System.out.println("Por favor, digite o número da Agência: ");
+		String agencia = sc.next();
+		sc.nextLine();
+
+		System.out.println("Por favor, digite o seu nome: ");
+		String nomeCliente = sc.nextLine();
+
+		System.out.println("Por favor, faça um depósito na sua conta: ");
+		double saldo = sc.nextDouble();
+
+		System.out.println("Olá " + nomeCliente + ", " + "obrigado por criar uma conta em nosso banco,"
+				+ " sua agência é " + agencia + ", " + "conta " + numero + " e o seu saldo de " + saldo
+				+ " Já está disponível para saque!");
+
+		sc.close();
 
 	}
 
